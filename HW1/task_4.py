4e20a55f92a18e18d9b4a4cfcd59a589a27cd8ec
anatol <anatolminor91@inbox.ru>
2021-09-19 16:12:49 +0300

    test code

--- HW1/task_4.py
+++
@@ -1,5 +0,0 @@
-list = ["разработка", "администрирование", "protocol", "standard"]
-
-for i in list:
-    encode = i.encode(encoding="utf-8")
-    print( f'ENCODE - {encode} / DECODE - {encode.decode("utf-8")} ')
--- HW1/task_6.py
+++
@@ -1,6 +0,0 @@
-with open('test_file.txt', "r") as f:
-    print(f)
-
-with open('test_file.txt', "r", encoding='utf-8') as f:
-    for i in f:
-        print(i)
